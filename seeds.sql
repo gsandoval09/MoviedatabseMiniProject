fa60053aa578ccb098a8ac95e415ab4fd2223af8
George Sandoval <gssandoval09@gmail.com>
2021-10-22 15:18:38 -0500

    added code to database and schema

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies (movie_name)
+VALUES ("The Iron Giant"),
+       ("The Great Gatsby"),
+       ("Wolf of Wall Street"),
+       ("The Avengers"),
+       ("Harry Potter");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "Good"),
+       (2,  "Great"),
+       (3, "Amazing"),
+       (4,  "Exciting"),
+       (5, "Fun");
+      
