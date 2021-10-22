929f43294bc4b3f237e5aff646d33348af4d83e7
George Sandoval <gssandoval09@gmail.com>
2021-10-22 16:06:36 -0500

    succesfully connected to insomnia

--- server.js
+++ server.js
@@ -6,35 +6,52 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password
-    password: 'password',
-    database: 'movies_db'
-  },
-  console.log(`Connected to the movies_db database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password
+        password: 'password',
+        database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
 );
 
-app.get("/api/movies", (req,res)=>{
+app.get("/api/movies", (req, res) => {
     db.query('SELECT * FROM movies', function (err, results) {
         res.json(results)
-      });
-      
+    });
+
 })
 
-app.post("/api/add-movie", (req,res)=>{
-    db.query('SELECT * FROM movies', function (err, results) {
-        res.json(results)
-      });
-      
+app.post("/api/add-movie", (req, res) => {
+    console.log(req.body);
+    const { movie_title } = req.body
+    if (movie_title) {
+        const newmovie = {
+            movie_title
+        }
+        db.query(`insert into movies (movie_name) values (${newmovie.movie_title}) `, function (err, results) {
+            console.log(results)
+            
+            const response = {
+                status: "success",
+                body: newmovie
+            }
+            res.json(response)
+        });
+    } else {
+        res.json("error adding movie")
+    }
+
 })
+
+
 // db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
 //   if (err) {
 //     console.log(err);
@@ -44,18 +61,18 @@ app.post("/api/add-movie", (req,res)=>{
 
 // Query database
 db.query('SELECT * FROM movies', function (err, results) {
-  console.log(results);
+    console.log(results);
 });
 
 db.query('SELECT * FROM reviews', function (err, results) {
     console.log(results);
-  });
+});
 
-  // Default response for any other request (Not Found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
